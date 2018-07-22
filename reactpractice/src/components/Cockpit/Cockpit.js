fdd56dcf1f16cd11f21130ab08bd0979706e2d66
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-07-21 21:52:25 -0500

    created a cockpit component ment to control the button

--- reactpractice/src/components/Cockpit/Cockpit.js
+++ reactpractice/src/components/Cockpit/Cockpit.js
@@ -1,15 +1,33 @@
 import React from 'react';
 
-const CockPit = ( props ) => {
+import classes from './Cockpit.css';
+
+const Cockpit = ( props ) => {
+
+    const assignedClasses = [];
+
+    let btnClass = '';
+
+    if (props.showPersons) {
+        btnClass = classes.Red;
+    }
+
+    if (props.persons.length <= 2) {
+    assignedClasses.push(classes.red);
+    }
+    if (props.persons.length <=1) {
+    assignedClasses.push(classes.bold);
+    }
+
     return(
-        <div>
+        <div className={classes.Cockpit}>
             <h1> Hi, im a new react app </h1>
             <p className={assignedClasses.join(' ')} > this is realy working</p>
             <button
                 className={btnClass}
-                onClick={this.togglePersonsHandler}>Switch Name</button>
+                onClick={props.clicked}>Switch Name</button>
         </div>
     );
 };
 
-export default CockPit;
+export default Cockpit;
