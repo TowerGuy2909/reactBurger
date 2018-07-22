16c7551b9ae63880d99577b8b124f18058c4b516
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-07-21 22:50:11 -0500

    turned functional component to a statefull component

--- reactpractice/src/components/Persons/Persons.js
+++ reactpractice/src/components/Persons/Persons.js
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Person from './Person/Person';
 import ErrorBoundary from '../../ErrorBoundary/ErrorBoundary';
 
-const Persons = (props) => (props.persons.map((person, index) => {
-        return <ErrorBoundary key={person.id}>
-          <Person
-            click={() => props.clicked(index)} 
-            name={person.name} 
-            age={person.age}
-            changed={(event) => props.changed(event, person.id)}/>
-          </ErrorBoundary>
-        
-      } ));
+class Persons extends Component {
+    render() {
+        return this.props.persons.map((person, index) => {
+            return <ErrorBoundary key={person.id}>
+              <Person
+                click={() => this.props.clicked(index)} 
+                name={person.name} 
+                age={person.age}
+                changed={(event) => this.props.changed(event, person.id)}/>
+              </ErrorBoundary>
+        });
+    }   
+}   
 
 export default Persons;
