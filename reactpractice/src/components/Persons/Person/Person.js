b4596b754332bc1f745e9906fb953204107f8d78
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-07-22 22:44:46 -0500

    lifecycle demo

--- reactpractice/src/components/Persons/Person/Person.js
+++ reactpractice/src/components/Persons/Person/Person.js
@@ -3,7 +3,20 @@ import React, { Component } from 'react';
 import classes from './Person.css'
 
 class Person extends Component {
+    constructor(props){
+        super(props);
+        console.log('[Person.js] Inside the Constructor', props);
+      }
+    
+      componentWillMount() {
+        console.log('[Person.js] Inside the componentWillMount');
+      }
+    
+      componentDidMount(){
+        console.log('[Person.js] Inside the componentDidMount');
+      }
     render(){
+        console.log('[Person.js] Inside render()')
         return (
             <div className={classes.Person} >
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
