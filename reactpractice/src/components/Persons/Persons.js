591989303a4598a14f836c74510a386c2be86e61
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-07-22 22:44:34 -0500

    lifecycle demo

--- reactpractice/src/components/Persons/Persons.js
+++ reactpractice/src/components/Persons/Persons.js
@@ -1,10 +1,44 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 
 import Person from './Person/Person';
 import ErrorBoundary from '../../ErrorBoundary/ErrorBoundary';
 
-class Persons extends Component {
+class Persons extends PureComponent {
+    constructor(props){
+        super(props);
+        console.log('[Persons.js] Inside the Constructor', props);
+      }
+    
+      componentWillMount() {
+        console.log('[Persons.js] Inside the componentWillMount');
+      }
+    
+      componentDidMount(){
+        console.log('[Persons.js] Inside the componentDidMount');
+      }
+
+      componentWillReceiveProps (nextProps) {
+          console.log('[UPDATE Persons.js] Inside componentWillReceiveProps', nextProps);
+      }
+
+      shouldComponentUpdate (nextProps, nextState) {
+        console.log('[UPDATE Persons.js] Inside shouldComponentUpdate', nextProps, nextState);
+        return nextProps.persons !== this.props.persons || 
+        nextProps.changed !== this.props.changed ||
+        nextProps.clicked !== this.props.clicked;
+        // return true;
+      }
+
+      componentWillUpdate (nextProps, nextState) {
+        console.log('[UPDATE Persons.js] Inside componentWillUpdate', nextProps, nextState);
+      }
+
+      componentDidUpdate (nextProps, nextState) {
+        console.log('[UPDATE Persons.js] Inside componentDidUpdate', nextProps, nextState);
+      }
+
     render() {
+        console.log('[Persons.js] Inside render()')
         return this.props.persons.map((person, index) => {
             return <ErrorBoundary key={person.id}>
               <Person
